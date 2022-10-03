b4110db932fa8930a237b2f51e2a45c92335cfeb
Mridul Bagra <89296921+mridulbagra@users.noreply.github.com>
2022-10-03 21:14:27 +0530

    Update repository.dart

--- lib/resources/repository.dart
+++ lib/resources/repository.dart
@@ -1,7 +1,7 @@
-import 'package:forecast/models/weather_forecast_model.dart';
+import 'package:forecasting/models/weather_forecast_model.dart';
 
 import 'openweathermap_api_provider.dart';
-import 'package:forecast/models/weather_model.dart';
+import 'package:forecasting/models/weather_model.dart';
 
 /// Code for BLoC Pattern referred from a Medium post.
 /// See <https://medium.com/flutterpub/architecting-your-flutter-project-bd04e144a8f1> for source.
